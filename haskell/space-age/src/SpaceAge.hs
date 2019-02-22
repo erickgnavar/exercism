1c5780e74aabe407ae012c69929f5262e6880841
Erick Navarro <erick@navarro.io>
2019-02-21 23:49:42 -0800

    Improve haskell spaceage exercise

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -10,15 +10,14 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds =
-  case planet of
-    Mercury -> applyFactor 0.2408467
-    Venus -> applyFactor 0.61519726
-    Earth -> applyFactor 1
-    Mars -> applyFactor 1.8808158
-    Jupiter ->applyFactor 11.862615
-    Saturn -> applyFactor 29.447498
-    Uranus -> applyFactor 84.016846
-    Neptune -> applyFactor 164.79132
-    where earthSeconds = 31557600
-          applyFactor factor = (seconds / earthSeconds) / factor
+ageOn planet seconds = ageOnEarth / factor
+  where ageOnEarth = seconds / 31557600
+        factor = case planet of
+          Mercury ->  0.2408467
+          Venus ->  0.61519726
+          Earth ->  1
+          Mars ->  1.8808158
+          Jupiter -> 11.862615
+          Saturn ->  29.447498
+          Uranus ->  84.016846
+          Neptune ->  164.79132
