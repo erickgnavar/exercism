1462153ed7904564ab8452ca231b7250adb7e49c
Erick Navarro <erick@navarro.io>
2023-01-08 09:56:13 -0800

    [emacs-lisp] Hello world

---
+++ emacs-lisp/hello-world/hello-world.el
@@ -0,0 +1,10 @@
+;;; hello-world.el --- Hello World exercise (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun hello ()
+  "Hello world function."
+  "Hello, World!")
+
+(provide 'hello-world)
+;;; hello-world.el ends here
